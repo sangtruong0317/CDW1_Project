d9bb2605b6e7e8ca8f35a9319782a143a6177914
sangtm97 <minhsangtrieu94@gmail.com>
2021-10-19 13:34:26 +0700

    update addNewProtype

--- admin/models/protype.php
+++ admin/models/protype.php
@@ -10,5 +10,14 @@ class Protype extends Db
         $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
         return $items; //return an array
     }
+
+    //Viet phuong thêm 1 protype
+    function addNewProtype($type_name)
+    {
+        $sql = self::$connection->prepare("INSERT INTO `protypes`(`type_name`) 
+        VALUES (?)");
+        $sql->bind_param("s", $type_name);
+        $sql->execute(); //return an object
+    }
 }
 ?>
