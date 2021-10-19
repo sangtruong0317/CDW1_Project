ea53c86f03d29eba7a2ed37074cc9f0063ef9070
sangtruong0317 <truonggioisang.tdc2019@gmail.com>
2021-10-19 16:34:01 +0700

    update footer

--- form/footer.php
+++ form/footer.php
@@ -1,13 +1,63 @@
-<footer id="footer">
-        <!--Footer-->
+<!-- <footer id="footer">
+        
 
         <div class="footer-bottom">
             <div class="container">
-                <div class="row">
-                    <!-- <p class="pull-left"><a href="./Login/logout.php">đăng nhập</a></p> -->
+                <div class="row">                 
                     <p class="pull-center">Designed by Group G</p>
                 </div>
             </div>
         </div>
-    </footer>
-    <!--/Footer-->
+    </footer> -->
+    
+  
+ <footer class="bg-white">
+     <div class="container py-5">
+         <div class="row py-3">
+             <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
+                 <h3 class="text-uppercase font-weight-bold mb-4">About</h3>
+                 <ul class="list-unstyled mb-0">
+                     <li class="mb-2"><a href="#" class="text-muted">Contact Us</a></li>
+                     <li class="mb-2"><a href="#" class="text-muted">About Us</a></li>
+                     <li class="mb-2"><a href="#" class="text-muted">Stories</a></li>
+                     <li class="mb-2"><a href="#" class="text-muted">Press</a></li>
+                 </ul>
+             </div>
+             <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
+                 <h3 class="text-uppercase font-weight-bold mb-4">Help</h3>
+                 <ul class="list-unstyled mb-0">
+                     <li class="mb-2"><a href="#" class="text-muted">Payments</a></li>
+                     <li class="mb-2"><a href="#" class="text-muted">Shipping</a></li>
+                     <li class="mb-2"><a href="#" class="text-muted">Cancellation</a></li>
+                     <li class="mb-2"><a href="#" class="text-muted">Returns</a></li>
+                 </ul>
+             </div>           
+             <div class="col-lg-2 col-md-6 mb-4 mb-lg-0">
+                 <h3 class="text-uppercase font-weight-bold mb-4">Company</h3>
+                 <ul class="list-unstyled mb-0">
+                     <li class="mb-2"><a href="#" class="text-muted">Login</a></li>
+                     <li class="mb-2"><a href="#" class="text-muted">Register</a></li>
+                     <li class="mb-2"><a href="#" class="text-muted">Sitemap</a></li>
+                     <li class="mb-2"><a href="#" class="text-muted">Our Products</a></li>
+                 </ul>
+             </div>
+             <div class="col-lg-4 col-md-6 mb-lg-0">
+                 <h3 class="text-uppercase font-weight-bold mb-4">Registered Office Address</h3>
+                 <p class="text-muted mb-4">Here , write the complete address of the Registered office address along with telephone number.</p>
+                 <ul class="list-inline mt-4">
+                     <li class="list-inline-item"><a href="#" target="_blank" title="twitter"><i class="fab fa-2x fa-twitter"></i></a></li>
+                     <li class="list-inline-item"><a href="#" target="_blank" title="facebook"><i class="fab fa-2x fa-facebook-f"></i></a></li>
+                     <li class="list-inline-item"><a href="#" target="_blank" title="instagram"><i class="fab fa-2x fa-instagram"></i></a></li>
+                     <li class="list-inline-item"><a href="#" target="_blank" title="pinterest"><i class="fab fa-2x fa-youtube"></i></a></li>
+                     <li class="list-inline-item"><a href="#" target="_blank" title="vimeo"><i class="fab fa-2x fa-google"></i></a></li>
+                 </ul>
+             </div>
+         </div>
+     </div>
+     <hr class="p-0 m-0 b-0">
+     <div class="bg-light py-2">
+         <div class="container text-center">
+             <p class="text-muted mb-0 py-2">© 2021 Designed by Group G</p>
+         </div>
+     </div>
+ </footer>
