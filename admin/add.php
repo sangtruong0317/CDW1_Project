f660a3154a8ed0a59b26110d25e0fdb02a319ed6
sangtm97 <minhsangtrieu94@gmail.com>
2021-10-19 14:14:34 +0700

    update add.php

--- admin/add.php
+++ admin/add.php
@@ -14,7 +14,7 @@
             if(!file_exists($path)){
                 if(move_uploaded_file($_FILES['fileUpload']['tmp_name'],$path)){
                     $product->addNewProduct($name, $manu_id, $type_id, $price, $pro_image, $description, $feature, $created_at);
-                    echo "<script>alert('Đã thêm');window.location.href='index.php'</script>";
+                    echo "<script>alert('Thêm thành công');window.location.href='index.php'</script>";
                 }
             }
             else{
@@ -22,17 +22,17 @@
             }
         }
         else{
-            echo "<script>alert('File được chọn không phải là ảnh')";
+            echo "<script>alert('File được chọn không phải là file ảnh')";
         }
        // header("location:index.php");
     } if (isset($_POST['addManufacture'])) {
 
         $manu_name = $_POST['name'];
         $manufacture->addNewManufacture($manu_name);
-        echo "<script>alert('Đã thêm');window.location.href='manufactures.php'</script>";
+        echo "<script>alert('Thêm thành công');window.location.href='manufactures.php'</script>";
     } if (isset($_POST['addProtype'])) {
 
         $type_name = $_POST['name'];
         $protype->addNewProtype($type_name);
-        echo "<script>alert('Đã thêm');window.location.href='protypes.php'</script>";
+        echo "<script>alert('Thêm thành công');window.location.href='protypes.php'</script>";
     }
