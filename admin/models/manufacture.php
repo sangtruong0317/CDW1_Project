91d21fddbe882ea727ee0f36ac9036f545942e37
sangtm97 <minhsangtrieu94@gmail.com>
2021-10-20 09:28:32 +0700

    update

--- admin/models/manufacture.php
+++ admin/models/manufacture.php
@@ -34,4 +34,31 @@ class Manufacture extends Db
         $sql->execute(); //return an object
     }
     
+    //Viet phuong thuc xóa 1 manufacture
+    function delManufactuere($manu_id)
+    {
+        $sql = self::$connection->prepare("DELETE
+    FROM manufactures
+    WHERE manu_id = ?");
+        $sql->bind_param("i", $manu_id);
+        $sql->execute(); //return an object
+    }
+
+    //Viet phuong thuc sua sp
+    function updateManu($id,$name){
+        $query = self::$connection->prepare("UPDATE manufactures SET manu_name = ? WHERE manu_id = ?");
+        $query->bind_param("si",$name,$id);
+         //return an object	 
+        return $query->execute(); //return an array
+    }
+
+    //Lay san bang id
+    function getManuID($id){
+        $query = self::$connection->prepare("SELECT *   FROM manufactures WHERE manu_id = ?");
+        $query->bind_param("i",$id);
+        $query->execute(); //return an object	 
+        $items = array();
+        $items = $query->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array	   
+    }
 }
--- admin/models/protype.php
+++ admin/models/protype.php
@@ -19,5 +19,34 @@ class Protype extends Db
         $sql->bind_param("s", $type_name);
         $sql->execute(); //return an object
     }
+
+    //Viet phuong thuc xóa 1 protype
+    function delProtype($type_id)
+    {
+        $sql = self::$connection->prepare("DELETE
+    FROM protypes
+    WHERE type_id = ?");
+        $sql->bind_param("i", $type_id);
+        $sql->execute(); //return an object
+    }
+    
+    //update
+    function updateProtype($id,$typeName){
+        $query = self::$connection->prepare("UPDATE protypes SET type_name = ? WHERE type_id = ?");
+        $query->bind_param("si",$typeName,$id);
+        return $query->execute();
+    }
+
+    
+    //lay id 
+    function getProtypeID($id){
+        $sql = self::$connection->prepare("SELECT * FROM protypes WHERE type_id = ?");
+        $sql->bind_param("i", $id);
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items;
+    }
+    
 }
 ?>
