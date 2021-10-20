95383d353607fe991c93f6479ded212347206adc
sangtruong0317 <truonggioisang.tdc2019@gmail.com>
2021-10-20 15:33:36 +0700

    add feature

--- models/product.php
+++ models/product.php
@@ -11,15 +11,22 @@ class Product extends Db
         return $items; //return an array
     }
     //Viet phuong thuc lay ra 3 sp noi bat
-function getAllFeatureProducts(){
+    function getFeatureProducts(){
     $sql = self::$connection->prepare("SELECT * FROM products WHERE feature = 1 LIMIT 3 ");
     $sql->execute();//return an object
     $items = array();
     $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
     return $items; //return an array
-}
-//Viet phuong thuc lay ra 3 sp moi nhat
-
+    }
+    //Viet phuong thuc lay ra 6 sp moi nhat
+    function getNewProducts()
+    {
+        $sql = self::$connection->prepare("SELECT * FROM products WHERE products.Created_at ORDER by Created_at DESC LIMIT 1,6");
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
     //Hàm viết ra danh sách tất cả sản phẩm (phân trang) 
     
     //Viet phuong thuc lay ra san pham theo ID
