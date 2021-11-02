e1592dddc3d1873df4d282e505aac457a8358ad7
PhanNhatQuy <phannhatquy.tdc2019@gmail.com>
2021-11-02 11:55:32 +0700

    create lay san pham theo loai

--- models/protype.php
+++ models/protype.php
@@ -9,7 +9,13 @@ class Protype extends Db{
         return $items; //return an array
     }
     //Viet phuong thuc lay ra tat ca san pham theo type_id
-
+    function getProtypeByID($type_id){
+        $sql = self::$connection->prepare("SELECT * FROM products,protypes WHERE products.type_ID = '$type_id' and products.type_ID = protypes.type_id");
+        $sql->execute();//return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
     //phân trang
     
 }
