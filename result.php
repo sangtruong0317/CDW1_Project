a5935c1aaa89666f0c3768b75bd8e056a355a530
sangtruong0317 <truonggioisang.tdc2019@gmail.com>
2021-11-09 15:53:16 +0700

    update fixUI

--- result.php
+++ result.php
@@ -21,7 +21,7 @@
                 <div class="row">
                     <div class="col-sm-3">
                         <div class="left-sidebar">
-                            <h2>Category</h2>
+                            <h2>Thương Hiệu</h2>
                             <div class="panel-group category-products" id="accordian">
                                 <!--category-productsr-->
                                 <div class="panel panel-default">
