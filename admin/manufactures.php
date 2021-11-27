cd50ba674446ed74655aa3062d3e8aa6a3c8a540
Ho Hoang Phu <hohoangphu.tdc2019@gmail.com>
2021-11-27 13:38:46 +0700

    Create add manu

--- admin/manufactures.php
+++ admin/manufactures.php
@@ -57,11 +57,12 @@ $total = count($manufacture->getAllManufactures());     // Tính tổng số dò
                                     </tr>
                                 </thead>
                                 <tbody>
+                                <?php foreach ($manufacture->getAllManufactureByPage($page, $perPage) as $value) { ?>
                                         <tr class="">
-                                            <td></td>
-                                            <td></td>
+                                            <td><?php echo $value['manu_id'] ?></td>
+                                            <td><?php echo $value['manu_name'] ?></td>
                                             <td>
-                                                <a href="#" class="btn btn-primary" style="margin: 0px 0px 0 100px">Thêm <i class="fas fa-plus-square"></i></a>
+                                                <a href="form.php?value=manufacture" class="btn btn-primary" style="margin: 0px 0px 0 100px">Thêm <i class="fas fa-plus-square"></i></a>
                                                 <button type="button" class="btn btn-danger" 
                                                 data-toggle="modal" data-target="#delete"
                                                 style="margin: 0 20px 0 20px">
@@ -71,8 +72,16 @@ $total = count($manufacture->getAllManufactures());     // Tính tổng số dò
                                                 <a href="edit.php?idManu=<?= $value['manu_id'] ?>&editManu" class="btn btn-success">Sửa <i class="far fa-edit"></i></a>
                                             </td>
                                         </tr>
+                                        <?php } ?>
                                 </tbody>
                             </table>
+                             <div class="row" style="margin-left: 18px;">
+                                <ul class="pagination">
+                                    <div class="active">
+                                    <?php echo $db->paginate($url, $total, $page, $perPage) ?>
+                                    </div>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
